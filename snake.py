26d9e77a99df500c3201359dec5b83b9f0d5297e
Kishore kumar.RP <43315290+kiskumd@users.noreply.github.com>
2018-09-28 16:31:16 +0530

    Update snake.py

--- snake.py
+++ snake.py
@@ -47,3 +47,63 @@ while(count<=100):
 	elif(count==100):
 		print("u won the game")
 		break
+OUTPUT
+enter 'r' to roll a dice 'q' to quitr
+my position  3
+your random value 3
+enter 'r' to roll a dice 'q' to quitr
+my position  6
+your random value 3
+enter 'r' to roll a dice 'q' to quitr
+my position  12
+your random value 6
+enter 'r' to roll a dice 'q' to quitr
+my position  13
+your random value 1
+congrats u got a ladder
+enter 'r' to roll a dice 'q' to quitr
+my position  38
+your random value 4
+oops snake bite
+enter 'r' to roll a dice 'q' to quitr
+my position  44
+your random value 6
+enter 'r' to roll a dice 'q' to quitr
+my position  47
+your random value 3
+enter 'r' to roll a dice 'q' to quitr
+my position  52
+your random value 5
+congrats u got a ladder
+enter 'r' to roll a dice 'q' to quitr
+my position  87
+your random value 6
+enter 'r' to roll a dice 'q' to quitr
+my position  93
+your random value 6
+oops snake bite
+enter 'r' to roll a dice 'q' to quitr
+my position  69
+your random value 5
+enter 'r' to roll a dice 'q' to quitr
+my position  74
+your random value 5
+enter 'r' to roll a dice 'q' to quitr
+my position  75
+your random value 1
+enter 'r' to roll a dice 'q' to quitr
+my position  76
+your random value 1
+congrats u got a ladder
+enter 'r' to roll a dice 'q' to quitr
+my position  101
+your random value 4
+u cant go beyond 100
+enter 'r' to roll a dice 'q' to quitr
+my position  103
+your random value 6
+u cant go beyond 100
+enter 'r' to roll a dice 'q' to quitr
+my position  100
+your random value 3
+u won the game
